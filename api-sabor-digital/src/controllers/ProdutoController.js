961f4ef9e2ed74286c9d5d693e983eefa49f1afb
livia-hermano <livia.hermano@aluno.senai.br>
2026-05-06 07:46:02 -0300

    conclusão da ProdutoController

--- api-sabor-digital/src/controllers/ProdutoController.js
+++ api-sabor-digital/src/controllers/ProdutoController.js
@@ -25,4 +25,40 @@ class ProdutoController{
                 erro: erro 
         })
     }
-}}
+}
+    async cadastrar(req, res){
+        try{
+            const resultado = await ProdutoService.cadastrarProduto(req.body)
+            res.status(201).json(resultado)
+        }catch (erro){
+            res.status(500).json({
+                sucesso: false,
+                mensagem: error.mensagem || 'Erro interno do servidor',
+                erro: erro })
+        }
+    }
+    async atualizar(req, res){
+        try{
+            const resultado = await ProdutoService.atualizarProduto(req.params.id, req.body)
+            res.status(200).json(resultado)
+        }catch (erro){
+            res.status(500).json({
+                sucesso: false,
+                mensagem: error.mensagem || 'Erro interno do servidor',
+                erro: erro })
+        }
+    }
+    async deletar(req, res){
+        try{
+            const resultado = await ProdutoService.deletarProduto(req.params.id)
+            res.status(200).json(resultado)
+        }catch (erro){
+            res.status(500).json({
+                sucesso: false,
+                mensagem: error.mensagem || 'Erro interno do servidor',
+                erro: erro })
+        }
+    }
+}
+
+module.exports = new ProdutoController()
